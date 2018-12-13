85f1337fc6a47e5b7d765117379fbfc4f70b21d8
Christopher Pace <myiahsade@gmail.com>
2018-12-13 09:32:06 -0600

    check in

---
+++ answer.py
@@ -0,0 +1,17 @@
+# def numbers(input):
+#     for i in range(input):
+#         print(i)
+def listsum(num):
+    Sum = 0
+    for i in range(num+1):
+        print(i)
+        Sum += i
+    print("The sum is: ", Sum)
+
+
+
+# s = ()
+# b = sum(s)
+# print(b)
+# numbers(4)
+listsum(4)
